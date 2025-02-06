a80889890fbda2f1d7904edb044308ae0f70ed82
abby-1b <abby.041b@gmail.com>
2025-02-06 09:55:13 -0400

    Added classes/styling to dynamic tab system

--- src/dynamic-tabs.ts
+++ src/dynamic-tabs.ts
@@ -1,10 +1,10 @@
 
 type UserInput =
-  | { label: string, onChange?: (this: HTMLElement) => void, type: 'text' }
-  | { label: string, onChange?: (this: HTMLElement) => void, type: 'number' }
-  | { label: string, onChange?: (this: HTMLElement) => void, type: 'select', options: string[] | Record<string, any> }
+  | { label: string, onChange?: (this: GlobalEventHandlers) => void, type: 'text' }
+  | { label: string, onChange?: (this: GlobalEventHandlers) => void, type: 'number' }
+  | { label: string, onChange?: (this: GlobalEventHandlers) => void, type: 'select', options: string[] | Record<string, any> }
 
-interface Tab {
+export interface Tab {
   name: string,
   inner: UserInput[],
 }
@@ -27,13 +27,16 @@ function renderTabHeader(
 ): HTMLElement {
   const tabElements = [...tabScroller.children] as HTMLElement[];
   function changeTab(index: number) {
+    console.log('changing tab:', index);
     for (const tab of tabElements) {
       tab.style.left = '-' + (index * 100) + 'vw';
     }
   }
   const final = document.createElement('div');
+  final.className = 'tab-select-holder';
   names.forEach((name, index) => {
     const tab = document.createElement('input');
+    tab.type = 'button';
     tab.value = name;
     tab.onclick = () => changeTab(index);
     final.appendChild(tab);
@@ -50,7 +53,12 @@ function renderTabScroller(tabs: HTMLElement[]): HTMLElement {
 }
 
 function renderSingleTab(tab: Tab): HTMLElement {
-  tab.name
+  const el = document.createElement('div');
+  el.className = 'tab-content';
+
+  tab.inner.forEach(input => el.appendChild(renderUserInput(input)));
+
+  return el;
 }
 
 function renderUserInput(input: UserInput): HTMLElement {
@@ -58,17 +66,20 @@ function renderUserInput(input: UserInput): HTMLElement {
 
   const label = document.createElement('h3')
   label.innerText = input.label;
+  label.className = 'text-label';
   final.appendChild(label);
 
   let el: HTMLInputElement | HTMLSelectElement;
   switch (input.type) {
     case 'text': {
       el = document.createElement('input');
-      input.type = 'text';
+      el.className = 'text-input';
+      el.type = 'text';
     } break;
     case 'number': {
       el = document.createElement('input');
-      input.type = 'number';
+      el.className = 'number-input';
+      el.type = 'number';
     } break;
     case 'select': {
       el = document.createElement('select');
@@ -82,5 +93,9 @@ function renderUserInput(input: UserInput): HTMLElement {
       }
     } break;
   }
-  el.onchange = input.onChange;
+  el.onchange = input.onChange ?? null;
+
+  final.appendChild(el);
+
+  return final;
 }
