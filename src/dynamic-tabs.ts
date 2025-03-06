b46284449836fe09707dfb76ac9c6cd025846612
abby-1b <abby.041b@gmail.com>
2025-03-06 11:08:19 -0400

    small changes

--- src/dynamic-tabs.ts
+++ src/dynamic-tabs.ts
@@ -3,83 +3,186 @@ type UserInput =
   | { label: string, onChange?: (this: GlobalEventHandlers) => void, type: 'text' }
   | { label: string, onChange?: (this: GlobalEventHandlers) => void, type: 'number' }
   | { label: string, onChange?: (this: GlobalEventHandlers) => void, type: 'select', options: string[] | Record<string, any> }
+  | { label: string, onChange?: (this: GlobalEventHandlers) => void, type: 'horizontal', inputs: UserInput[] }
 
-export interface Tab {
-  name: string,
-  inner: UserInput[],
-}
-
-export function renderTabs(tabs: Tab[]): HTMLElement {
-  const final = document.createElement('div');
-
-  const renderedTabs = tabs.map(renderSingleTab);
-  const tabScroller = renderTabScroller(renderedTabs);
-  const tabHeader = renderTabHeader(tabs.map(t => t.name), tabScroller);
-  final.appendChild(tabHeader);
-  final.appendChild(tabScroller);
-
-  return final;
-}
-
-function renderTabHeader(
-  names: string[],
-  tabScroller: HTMLElement
-): HTMLElement {
-  const tabElements = [...tabScroller.children] as HTMLElement[];
-  function changeTab(index: number) {
-    console.log('changing tab:', index);
-    for (const tab of tabElements) {
-      tab.style.left = '-' + (index * 100) + 'vw';
-    }
+export class Tab<T extends Record<string, any>> {
+  public element = document.createElement('div');
+  private inputs = new Map<string, any>();
+  private name: string = '';
+  public constructor() {
+    this.element.className = 'tab-content';
+    // inputs.forEach(input => this.element.appendChild(renderUserInput(input)));
   }
-  const final = document.createElement('div');
-  final.className = 'tab-select-holder';
-  names.forEach((name, index) => {
-    const tab = document.createElement('input');
-    tab.type = 'button';
-    tab.value = name;
-    tab.onclick = () => changeTab(index);
-    final.appendChild(tab);
-  });
-  changeTab(0);
-  return final;
+
+  public setName(name: string) {
+    this.name = name;
+  }
+
+  private renderLabel(labelString: string, hideLabel: boolean) {
+    if (hideLabel) return;
+    const label = document.createElement('h3');
+    label.innerText = labelString;
+    label.className = 'text-label';
+    this.element.appendChild(label);
+  }
+
+  public text<N extends string>(label: N, hideLabel = false): Tab<T & Record<N, string>> {
+    this.inputs.set(label, '');
+    this.renderLabel(label, hideLabel);
+    const textInput = document.createElement('input');
+    textInput.className = 'text-input';
+    textInput.type = 'text';
+    textInput.onchange = () => this.inputs.set(label, textInput.value);
+    this.element.appendChild(textInput);
+    return this;
+  }
+
+  public number<N extends string>(label: N, hideLabel = false): Tab<T & Record<N, number>> {
+    this.inputs.set(label, 0);
+    this.renderLabel(label, hideLabel);
+    const numberInput = document.createElement('input');
+    numberInput.className = 'number-input';
+    numberInput.type = 'number';
+    numberInput.onchange = () => this.inputs.set(label, numberInput.value);
+    this.element.appendChild(numberInput);
+    return this;
+  }
+
+  public select<N extends string, O>(label: N, options: string[] | Record<string, O>, hideLabel = false): Tab<T & Record<N, O>> {
+    const optionStrings = Array.isArray(options)
+      ? options
+      : Object.keys(options);
+    // TODO:
+    return this;
+  }
+
+  public horizontalNumbers<N extends string>(label: N, labels: string[], hideLabel = true): Tab<T & Record<N, number[]>> {
+    this.inputs.set(label, new Array(labels.length).fill(0));
+    this.renderLabel(label, hideLabel);
+    const horizElement = document.createElement('div');
+    horizElement.className = 'horizontal-container'
+    for (let i = 0; i < labels.length; i++) {
+      const label = document.createElement('h3');
+      label.innerText = labels[i];
+      label.className = 'text-label';
+      horizElement.appendChild(label);
+      const numberInput = document.createElement('input');
+      numberInput.className = 'number-input';
+      numberInput.type = 'number';
+      numberInput.onchange = () => this.inputs.get(label)[i] = numberInput.value;
+      horizElement.appendChild(numberInput);
+    }
+    this.element.appendChild(horizElement);
+    return this;
+  }
+
+  public getValue<K extends keyof T & string>(valueKey: K): T[K] {
+    if (!this.inputs.has(valueKey)) {
+      throw new Error(`Tab \`${this.name}\` doesn't have input \`${valueKey}\`!`);
+    }
+    return this.inputs.get(valueKey) as T[K];
+  }
 }
 
-function renderTabScroller(tabs: HTMLElement[]): HTMLElement {
-  const final = document.createElement('div');
-  final.className = 'tab-scroller';
-  tabs.forEach(tab => final.appendChild(tab));
-  return final;
+export class DynamicTabs<T extends Record<string, Record<string, any>>> {
+  private tabs = new Map<string, Tab<any>>();
+
+  public constructor(
+    public parentElement: HTMLElement
+  ) {}
+
+  public addTab<N extends string, K extends Record<string, any>>(tabName: N, tab: Tab<K>): DynamicTabs<T & Record<N, K>> {
+    this.tabs.set(tabName, tab);
+    tab.setName(tabName);
+    this.parentElement.appendChild(tab.element);
+    return this;
+  }
+
+  public getValue<TabName extends keyof T & string, InputName extends keyof T[TabName] & string>(
+    tabName: TabName,
+    inputName: InputName,
+  ): T[TabName][InputName] {
+    return this.tabs.get(tabName)!.getValue(inputName);
+  }
+
+  public goto(tabName: string) {
+
+  }
 }
 
-function renderSingleTab(tab: Tab): HTMLElement {
-  const el = document.createElement('div');
-  el.className = 'tab-content';
+// function renderTabs(tabs: Tab[]): HTMLElement {
+//   const final = document.createElement('div');
 
-  tab.inner.forEach(input => el.appendChild(renderUserInput(input)));
+//   const renderedTabs = tabs.map(renderSingleTab);
+//   const tabScroller = renderTabScroller(renderedTabs);
+//   const tabHeader = renderTabHeader(tabs.map(t => t.name), tabScroller);
+//   final.appendChild(tabHeader);
+//   final.appendChild(tabScroller);
 
-  return el;
-}
+//   return final;
+// }
+
+// function renderTabHeader(
+//   names: string[],
+//   tabScroller: HTMLElement
+// ): HTMLElement {
+//   const tabElements = [...tabScroller.children] as HTMLElement[];
+//   function changeTab(index: number) {
+//     console.log('changing tab:', index);
+//     for (const tab of tabElements) {
+//       tab.style.left = '-' + (index * 100) + 'vw';
+//     }
+//   }
+//   const final = document.createElement('div');
+//   final.className = 'tab-select-holder';
+//   names.forEach((name, index) => {
+//     const tab = document.createElement('input');
+//     tab.type = 'button';
+//     tab.value = name;
+//     tab.onclick = () => changeTab(index);
+//     final.appendChild(tab);
+//   });
+//   changeTab(0);
+//   return final;
+// }
+
+// function renderTabScroller(tabs: HTMLElement[]): HTMLElement {
+//   const final = document.createElement('div');
+//   final.className = 'tab-scroller';
+//   tabs.forEach(tab => final.appendChild(tab));
+//   return final;
+// }
 
 function renderUserInput(input: UserInput): HTMLElement {
   const final = document.createElement('div');
 
-  const label = document.createElement('h3')
-  label.innerText = input.label;
-  label.className = 'text-label';
-  final.appendChild(label);
+  if (input.label.length > 0) {
+    const label = document.createElement('h3')
+    label.innerText = input.label;
+    label.className = 'text-label';
+    final.appendChild(label);
+  }
 
-  let el: HTMLInputElement | HTMLSelectElement;
+  let el: HTMLInputElement | HTMLSelectElement | HTMLElement;
   switch (input.type) {
     case 'text': {
-      el = document.createElement('input');
-      el.className = 'text-input';
-      el.type = 'text';
+      const inputEl = document.createElement('input');
+      inputEl.className = 'text-input';
+      inputEl.type = 'text';
+      el = inputEl;
     } break;
     case 'number': {
-      el = document.createElement('input');
-      el.className = 'number-input';
-      el.type = 'number';
+      const inputEl = document.createElement('input');
+      inputEl.className = 'number-input';
+      inputEl.type = 'number';
+      el = inputEl;
+    } break;
+    case 'horizontal': {
+      el = document.createElement('div');
+      el.className = 'horizontal-container'
+      for (const inp of input.inputs) {
+        el.appendChild(renderUserInput(inp));
+      }
     } break;
     case 'select': {
       el = document.createElement('select');
--- src/script.ts
+++ src/script.ts
@@ -1,89 +1,87 @@
-import { renderTabs, Tab } from "./dynamic-tabs";
+import { DynamicTabs, Tab } from "./dynamic-tabs";
 
-const COMPOST_OPTIONS = [
-  "05-10-10",
-  "05-25-05",
-  "06-06-12",
-  "06-08-10",
-  "07-16-16",
-  "08-06-10",
-  "08-08-10",
-  "08-08-12",
-  "08-10-20",
-  "08-24-24",
-  "10-02-30",
-  "10-05-15",
-  "10-05-20",
-  "10-05-30",
-  "10-10-05",
-  "10-10-08",
-  "10-10-10",
-  "10-10-20",
-  "10-15-15",
-  "10-20-20",
-  "10-25-25",
-  "12-03-12",
-  "12-04-06",
-  "12-04-10",
-  "12-05-15",
-  "12-05-20",
-  "12-06-06",
-  "12-06-08",
-  "12-06-06",
-  "12-06-08",
-  "12-06-10",
-  "12-06-16",
-  "12-08-16",
-  "12-12-24",
-  "15-02-08",
-  "15-03-15",
-  "15-05-10",
-  "15-08-04",
-  "15-05-20",
-  "15-10-05",
-  "15-10-10",
-  "15-10-15",
-  "15-15-10",
-  "15-15-15",
-  "15-15-18",
-  "16-04-04",
-  "16-04-08",
-  "18-06-12",
-  "20-00-10",
-  "20-00-20",
-  "20-04-08",
-  "20-05-10",
-  "20-05-15",
-  "20-05-20",
-  "20-10-10",
-  "20-10-15",
-  "21-07-07",
-  "21-07-14",
-  "Ammonium Nitrate",
-  "Ammonium Nitrate Ammonia 37%",
-  "Ammonium Nitrate Ammonia 41%",
-  "Ammonium Polyphosphate 24%",
-  "Ammonium Polyphosphate 30%",
-  "Ammonium Polyphosphate 34%",
-  "Ammonium Polyphosphate 37%",
-  "Ammonium Sulfate",
-  "Aqua Ammonia 20%",
-  "Aqua Ammonia 24%",
-  "Calcium Ammonium Nitrate 17% ",
-  "Diammonium Phosphate",
-  "N Solution",
-  "Phosphoric Acid 52%",
-  "Phosphoric Acid 68%",
-  "Phosphoric Acid 75%",
-  "Potassium Cloride",
-  "Potassium Sulfate",
-  "Triple Superphospate",
-  "Urea",
-  "Urea Ammonium Nitrate 28%",
-  "Urea Ammonium Nitrate 32%",
-  "Urea Solution 20%",
-  "Urea Solution 23%",
-];
+const COMPOST_OPTIONS: Record<string, [number, number, number]> = {
+  "05-10-10": [  5, 10, 10 ],
+  "05-25-05": [  5, 25,  5 ],
+  "06-06-12": [  6,  6, 12 ],
+  "06-08-10": [  6,  8, 10 ],
+  "07-16-16": [  7, 16, 16 ],
+  "08-06-10": [  8,  6, 10 ],
+  "08-08-10": [  8,  8, 10 ],
+  "08-08-12": [  8,  8, 12 ],
+  "08-10-20": [  8, 10, 20 ],
+  "08-24-24": [  8, 24, 24 ],
+  "10-02-30": [ 10,  2, 30 ],
+  "10-05-15": [ 10,  5, 15 ],
+  "10-05-20": [ 10,  5, 20 ],
+  "10-05-30": [ 10,  5, 30 ],
+  "10-10-05": [ 10, 10,  5 ],
+  "10-10-08": [ 10, 10,  8 ],
+  "10-10-10": [ 10, 10, 10 ],
+  "10-10-20": [ 10, 10, 20 ],
+  "10-15-15": [ 10, 15, 15 ],
+  "10-20-20": [ 10, 20, 20 ],
+  "10-25-25": [ 10, 25, 25 ],
+  "12-03-12": [ 12,  3, 12 ],
+  "12-04-06": [ 12,  4,  6 ],
+  "12-04-10": [ 12,  4, 10 ],
+  "12-05-15": [ 12,  5, 15 ],
+  "12-05-20": [ 12,  5, 20 ],
+  "12-06-06": [ 12,  6,  6 ],
+  "12-06-08": [ 12,  6,  8 ],
+  "12-06-10": [ 12,  6, 10 ],
+  "12-06-16": [ 12,  6, 16 ],
+  "12-08-16": [ 12,  8, 16 ],
+  "12-12-24": [ 12, 12, 24 ],
+  "15-02-08": [ 15,  2,  8 ],
+  "15-03-15": [ 15,  3, 15 ],
+  "15-05-10": [ 15,  5, 10 ],
+  "15-08-04": [ 15,  8,  4 ],
+  "15-05-20": [ 15,  5, 20 ],
+  "15-10-05": [ 15, 10,  5 ],
+  "15-10-10": [ 15, 10, 10 ],
+  "15-10-15": [ 15, 10, 15 ],
+  "15-15-10": [ 15, 15, 10 ],
+  "15-15-15": [ 15, 15, 15 ],
+  "15-15-18": [ 15, 15, 18 ],
+  "16-04-04": [ 16,  4,  4 ],
+  "16-04-08": [ 16,  4,  8 ],
+  "18-06-12": [ 18,  6, 12 ],
+  "20-00-10": [ 20,  0, 10 ],
+  "20-00-20": [ 20,  0, 20 ],
+  "20-04-08": [ 20,  4,  8 ],
+  "20-05-10": [ 20,  5, 10 ],
+  "20-05-15": [ 20,  5, 15 ],
+  "20-05-20": [ 20,  5, 20 ],
+  "20-10-10": [ 20, 10, 10 ],
+  "20-10-15": [ 20, 10, 15 ],
+  "21-07-07": [ 21,  7,  7 ],
+  "21-07-14": [ 21,  7, 14 ],
+  "Ammonium Nitrate"             : [ 20,  0, 0 ],
+  "Ammonium Nitrate Ammonia 37%" : [ 37,  0, 0 ],
+  "Ammonium Nitrate Ammonia 41%" : [ 41,  0, 0 ],
+  "Ammonium Polyphosphate 24%"   : [  8, 24, 0 ],
+  "Ammonium Polyphosphate 30%"   : [  9, 30, 0 ],
+  "Ammonium Polyphosphate 34%"   : [ 10, 34, 0 ],
+  "Ammonium Polyphosphate 37%"   : [ 11, 37, 0 ],
+  "Ammonium Sulfate"             : [ 21,  0, 0 ],
+  "Aqua Ammonia 20%"             : [ 20,  0, 0 ],
+  "Aqua Ammonia 24%"             : [ 24,  0, 0 ],
+  "Calcium Ammonium Nitrate 17%" : [ 17,  0, 0 ],
+  "Diammonium Phosphate"         : [ 18, 46, 0 ],
+  "N Solution"                   : [ 30,  0, 0 ],
+  "Phosphoric Acid 52%"          : [  0, 52, 0 ],
+  "Phosphoric Acid 68%"          : [  0, 68, 0 ],
+  "Phosphoric Acid 75%"          : [  0, 75, 0 ],
+  "Potassium Cloride"            : [  0,  0, 6 ],
+  "Potassium Sulfate"            : [  0,  0, 5 ],
+  "Triple Superphospate"         : [  0, 45, 0 ],
+  "Urea"                         : [ 46,  0, 0 ],
+  "Urea Ammonium Nitrate 28%"    : [ 28,  0, 0 ],
+  "Urea Ammonium Nitrate 32%"    : [ 32,  0, 0 ],
+  "Urea Solution 20%"            : [ 20,  0, 0 ],
+  "Urea Solution 23%"            : [ 23,  0, 0 ],
+};
 
 const CROP_OPTIONS = [
   'Avocado',
@@ -119,36 +117,35 @@ const CROP_OPTIONS = [
   'Yam',
 ];
 
-const tabSiteAssesment: Tab = {
-  name: 'Input',
-  inner: [
-    { label: 'Cropping History', type: 'select', options: COMPOST_OPTIONS },
-    
-    // TODO: make these align side-by-side
-    { label: 'N', type: 'number' },
-    { label: 'P', type: 'number' },
-    { label: 'K', type: 'number' },
+const tabInput = new Tab<{}>()
+  .select('Cropping History', COMPOST_OPTIONS)
+  .horizontalNumbers('NPK', [ 'N', 'P', 'K' ], true)
+  .select('Preplant Soil Test', CROP_OPTIONS)
+  .number('Manure Test and Nitrogen Value')
+  .number('NCAT')
+  .text('Lol');
 
-    { label: 'Preplant Soil Test', type: 'select', options: CROP_OPTIONS },
-    { label: 'Manure Test and Nitrogen Value', type: 'number' },
-    { label: 'Nutrients Inventory', type: 'number' },
-    { label: 'NCAT', type: 'number' },
-  ]
-};
+const tabs = new DynamicTabs<{}>(document.body)
+  .addTab('Input', tabInput);
 
-const tabReport: Tab = {
-  name: 'Report',
-  inner: [
-    { label: 'test!', type: 'text' }
-  ]
-};
 
-const tabInfoOutput: Tab = {
-  name: 'Info Output',
-  inner: [
-    { label: 'test!', type: 'text' }
-  ]
-};
+// const tabReport: Tab = {
+//   name: 'Report',
+//   inner: [
+//     { label: 'test!', type: 'text' }
+//   ]
+// };
 
-const renderedTabs = renderTabs([ tabSiteAssesment, tabReport, tabInfoOutput ]);
-document.body.appendChild(renderedTabs);
+// const tabInfoOutput: Tab = {
+//   name: 'Info Output',
+//   inner: [
+//     { label: 'test!', type: 'text' }
+//   ]
+// };
+
+
+
+// const renderedTabs = renderTabs([ tabSiteAssesment, tabReport, tabInfoOutput ]);
+// document.body.appendChild(renderedTabs);
+
+
