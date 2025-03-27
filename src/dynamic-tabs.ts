dae97534bb4f6e5bec16d84df4c7ad98aba0fa58
abby-1b <abby.041b@gmail.com>
2025-03-27 14:09:53 -0400

    Fixed dynamic tabs + styling changes
    Overall, progress!

--- src/dynamic-tabs.ts
+++ src/dynamic-tabs.ts
@@ -1,10 +1,4 @@
 
-type UserInput =
-  | { label: string, onChange?: (this: GlobalEventHandlers) => void, type: 'text' }
-  | { label: string, onChange?: (this: GlobalEventHandlers) => void, type: 'number' }
-  | { label: string, onChange?: (this: GlobalEventHandlers) => void, type: 'select', options: string[] | Record<string, any> }
-  | { label: string, onChange?: (this: GlobalEventHandlers) => void, type: 'horizontal', inputs: UserInput[] }
-
 export class Tab<T extends Record<string, any>> {
   public element = document.createElement('div');
   private inputs = new Map<string, any>();
@@ -18,6 +12,14 @@ export class Tab<T extends Record<string, any>> {
     this.name = name;
   }
 
+  private inputChangeListeners: (() => void)[] = [];
+  public addInputListener(listener: () => void) {
+    this.inputChangeListeners.push(listener);
+  }
+  private inputWasChanged() {
+    this.inputChangeListeners.forEach(l => l());
+  }
+
   private renderLabel(labelString: string, hideLabel: boolean) {
     if (hideLabel) return;
     const label = document.createElement('h3');
@@ -26,37 +28,65 @@ export class Tab<T extends Record<string, any>> {
     this.element.appendChild(label);
   }
 
-  public text<N extends string>(label: N, hideLabel = false): Tab<T & Record<N, string>> {
-    this.inputs.set(label, '');
+  public text<N extends string>(label: N, hideLabel = false): Tab<T & Record<N, string | undefined>> {
     this.renderLabel(label, hideLabel);
     const textInput = document.createElement('input');
     textInput.className = 'text-input';
     textInput.type = 'text';
-    textInput.onchange = () => this.inputs.set(label, textInput.value);
+    textInput.onkeydown = () => {
+      this.inputs.set(
+        label,
+        textInput.value.length == 0 ? undefined : textInput.value
+      );
+      this.inputWasChanged();
+    };
     this.element.appendChild(textInput);
     return this;
   }
 
-  public number<N extends string>(label: N, hideLabel = false): Tab<T & Record<N, number>> {
-    this.inputs.set(label, 0);
+  public number<N extends string>(label: N, hideLabel = false): Tab<T & Record<N, number | undefined>> {
     this.renderLabel(label, hideLabel);
     const numberInput = document.createElement('input');
     numberInput.className = 'number-input';
     numberInput.type = 'number';
-    numberInput.onchange = () => this.inputs.set(label, numberInput.value);
+    numberInput.onkeydown = () => {
+      this.inputs.set(
+        label,
+        numberInput.value.length == 0 ? undefined : parseFloat(numberInput.value)
+      );
+      this.inputWasChanged();
+    };
     this.element.appendChild(numberInput);
     return this;
   }
 
-  public select<N extends string, O>(label: N, options: string[] | Record<string, O>, hideLabel = false): Tab<T & Record<N, O>> {
-    const optionStrings = Array.isArray(options)
-      ? options
-      : Object.keys(options);
-    // TODO:
+  public select<N extends string>(label: N, options: string[], hideLabel?: boolean): Tab<T & Record<N, string | undefined>>;
+  public select<N extends string, O>(label: N, options: Record<string, O>, hideLabel?: boolean): Tab<T & Record<N, O | undefined>>;
+  public select(label: string, options: string[] | Record<string, any>, hideLabel = false) {
+    this.renderLabel(label, hideLabel);
+    let optionStrings: string[] = [ '' ];
+    if (Array.isArray(options)) {
+      optionStrings.push(...options);
+    } else {
+      optionStrings.push(...Object.keys(options));
+    }
+    const selectInput = document.createElement('select');
+    selectInput.className = 'select-input';
+    optionStrings.forEach(o => {
+      const option = document.createElement('option');
+      option.text = o;
+      selectInput.appendChild(option);
+    });
+    selectInput.onchange = () => {
+      const val = Array.isArray(options) ? selectInput.value : (options[selectInput.value] ?? '');
+      this.inputs.set(label, val.length == 0 ? undefined : val);
+      this.inputWasChanged();
+    };
+    this.element.appendChild(selectInput);
     return this;
   }
 
-  public horizontalNumbers<N extends string>(label: N, labels: string[], hideLabel = true): Tab<T & Record<N, number[]>> {
+  public horizontalNumbers<N extends string, K extends [...string[]]>(label: N, labels: K, hideLabel = true): Tab<T & Record<N, { [J in keyof K]: number }>> {
     this.inputs.set(label, new Array(labels.length).fill(0));
     this.renderLabel(label, hideLabel);
     const horizElement = document.createElement('div');
@@ -69,18 +99,29 @@ export class Tab<T extends Record<string, any>> {
       const numberInput = document.createElement('input');
       numberInput.className = 'number-input';
       numberInput.type = 'number';
-      numberInput.onchange = () => this.inputs.get(label)[i] = numberInput.value;
+      numberInput.onkeydown = () => {
+        this.inputs.get(label)[i] = numberInput.value.length == 0
+          ? 0 : parseFloat(numberInput.value);
+        this.inputWasChanged();
+      };
       horizElement.appendChild(numberInput);
     }
     this.element.appendChild(horizElement);
     return this;
   }
 
-  public getValue<K extends keyof T & string>(valueKey: K): T[K] {
+  public tryGetValue<K extends keyof T & string>(valueKey: K): T[K] {
     if (!this.inputs.has(valueKey)) {
       throw new Error(`Tab \`${this.name}\` doesn't have input \`${valueKey}\`!`);
     }
     return this.inputs.get(valueKey) as T[K];
+  }
+  public getValue<K extends keyof T & string>(valueKey: K): NonNullable<T[K]> {
+    const val = this.tryGetValue(valueKey);
+    if (val === undefined || val === null) {
+      throw new Error(`This value is undefined!`);
+    }
+    return val;
   }
 }
 
@@ -94,6 +135,8 @@ export class DynamicTabs<T extends Record<string, Record<string, any>>> {
   public addTab<N extends string, K extends Record<string, any>>(tabName: N, tab: Tab<K>): DynamicTabs<T & Record<N, K>> {
     this.tabs.set(tabName, tab);
     tab.setName(tabName);
+
+    // TODO: multiple tabs
     this.parentElement.appendChild(tab.element);
     return this as DynamicTabs<T & Record<N, K>>;
   }
@@ -102,103 +145,20 @@ export class DynamicTabs<T extends Record<string, Record<string, any>>> {
     tabName: TabName,
     inputName: InputName,
   ): T[TabName][InputName] {
+    if (!this.tabs.has(tabName)) {
+      throw new Error(`Tab \`${tabName}\` not found!`);
+    }
     return this.tabs.get(tabName)!.getValue(inputName);
   }
 
+  public tryGetValue<TabName extends keyof T & string, InputName extends keyof T[TabName] & string>(
+    tabName: TabName,
+    inputName: InputName,
+  ): T[TabName][InputName] {
+    return this.tabs.get(tabName)?.tryGetValue(inputName);
+  }
+
   public goto(tabName: string) {
-
+    // TODO: switch tabs
   }
-}
-
-// function renderTabs(tabs: Tab[]): HTMLElement {
-//   const final = document.createElement('div');
-
-//   const renderedTabs = tabs.map(renderSingleTab);
-//   const tabScroller = renderTabScroller(renderedTabs);
-//   const tabHeader = renderTabHeader(tabs.map(t => t.name), tabScroller);
-//   final.appendChild(tabHeader);
-//   final.appendChild(tabScroller);
-
-//   return final;
-// }
-
-// function renderTabHeader(
-//   names: string[],
-//   tabScroller: HTMLElement
-// ): HTMLElement {
-//   const tabElements = [...tabScroller.children] as HTMLElement[];
-//   function changeTab(index: number) {
-//     console.log('changing tab:', index);
-//     for (const tab of tabElements) {
-//       tab.style.left = '-' + (index * 100) + 'vw';
-//     }
-//   }
-//   const final = document.createElement('div');
-//   final.className = 'tab-select-holder';
-//   names.forEach((name, index) => {
-//     const tab = document.createElement('input');
-//     tab.type = 'button';
-//     tab.value = name;
-//     tab.onclick = () => changeTab(index);
-//     final.appendChild(tab);
-//   });
-//   changeTab(0);
-//   return final;
-// }
-
-// function renderTabScroller(tabs: HTMLElement[]): HTMLElement {
-//   const final = document.createElement('div');
-//   final.className = 'tab-scroller';
-//   tabs.forEach(tab => final.appendChild(tab));
-//   return final;
-// }
-
-function renderUserInput(input: UserInput): HTMLElement {
-  const final = document.createElement('div');
-
-  if (input.label.length > 0) {
-    const label = document.createElement('h3')
-    label.innerText = input.label;
-    label.className = 'text-label';
-    final.appendChild(label);
-  }
-
-  let el: HTMLInputElement | HTMLSelectElement | HTMLElement;
-  switch (input.type) {
-    case 'text': {
-      const inputEl = document.createElement('input');
-      inputEl.className = 'text-input';
-      inputEl.type = 'text';
-      el = inputEl;
-    } break;
-    case 'number': {
-      const inputEl = document.createElement('input');
-      inputEl.className = 'number-input';
-      inputEl.type = 'number';
-      el = inputEl;
-    } break;
-    case 'horizontal': {
-      el = document.createElement('div');
-      el.className = 'horizontal-container'
-      for (const inp of input.inputs) {
-        el.appendChild(renderUserInput(inp));
-      }
-    } break;
-    case 'select': {
-      el = document.createElement('select');
-      const optionStrings = Array.isArray(input.options)
-        ? input.options as string[]
-        : Object.keys(input.options)
-      for (const option of optionStrings) {
-        const o = document.createElement('option');
-        o.innerText = option;
-        el.appendChild(o);
-      }
-    } break;
-  }
-  el.onchange = input.onChange ?? null;
-
-  final.appendChild(el);
-
-  return final;
 }
